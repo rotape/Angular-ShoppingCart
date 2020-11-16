53366574e7ec2d80ef14f5b0e4bf70536b76c7b7
Roger Tarragó <roger.correu@gmail.com>
2020-11-16 15:24:04 +0100

    first commit

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,8 +1,10 @@
 export const FireBaseConfig = {
-  apiKey: "<YOUR_API_KEY>",
-  authDomain: "<YOUR_AUTH_DOMAIN>",
-  databaseURL: "<YOUR_DATABASE_URL>",
-  projectId: "<YOUR_PROJECT_ID>",
-  storageBucket: "<YOUR_STORAGE_BUCKETS>",
-  messagingSenderId: "<YOUR_MESSAGING_SENDER_ID>",
+  apiKey: "AIzaSyD_oVn4WdwP6vk6NIniDOc7MQoYSzO4qSs",
+  authDomain: "augmented-world-191116.firebaseapp.com",
+  databaseURL: "https://augmented-world-191116.firebaseio.com",
+  projectId: "augmented-world-191116",
+  storageBucket: "augmented-world-191116.appspot.com",
+  messagingSenderId: "656353288075",
+  appId: "1:656353288075:web:3a8a6f3de2b6a51071198e",
+  measurementId: "G-NJ8V292EKS",
 };
